d66b35da19366457f5519b027476aea0b5b9160b
silviodantonio <64018191+silviodantonio@users.noreply.github.com>
2025-12-10 09:34:51 +0100

    Add app template

---
+++ app/webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    // mode can be eventually changed to "prod"
+    mode: 'development',
+    optimization: {
+        // Allegedly removes unused code, called tree-shaking
+        usedExports: true,
+    },
+    entry: path.resolve(__dirname, "src/script", "main.js"),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    // Include index.html file in bundle (./dist)
+    plugins: [new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src", "index.html")
+        }),
+    ],
+    // Enables loading of CSS in js files
+    module: {
+        rules: [
+            { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
+        ],
+    },
+    devServer: {
+        host: '0.0.0.0',
+        port: '80',
+    }
+};
