54e7cbb0b4bbeb565070592ffce10965c8d4585b
silviodantonio <64018191+silviodantonio@users.noreply.github.com>
2025-12-17 16:45:15 +0100

    Add bar chart

--- app/src/script/main.js
+++ app/src/script/main.js
@@ -2,38 +2,75 @@ import * as d3 from 'd3';
 
 import '../style/main.css';
 
-// Declare the chart dimensions and margins.
-const width = 640;
-const height = 400;
-const marginTop = 20;
-const marginRight = 20;
-const marginBottom = 30;
-const marginLeft = 40;
+const data = [
+    { label: 'A', value: 30 },
+    { label: 'B', value: 80 },
+    { label: 'C', value: 45 },
+    { label: 'D', value: 60 },
+    { label: 'E', value: 20 },
+    { label: 'F', value: 90 },
+    { label: 'G', value: 50 }
+];
 
-// Declare the x (horizontal position) scale.
-const x = d3.scaleUtc()
-    .domain([new Date("2023-01-01"), new Date("2024-01-01")])
-    .range([marginLeft, width - marginRight]);
+// Set up dimensions with margins for axes
+  const width = 640;
+  const height = 400;
+  const marginTop = 20;
+  const marginRight = 20;
+  const marginBottom = 30;
+  const marginLeft = 40;
 
-// Declare the y (vertical position) scale.
+const svg = d3.create('svg')
+    .attr('width', width)
+    .attr('height', height);
+
+// Create scales
+const x = d3.scaleBand()                        // Type of scale (ordinal, continuos, temporal... )
+    .domain(data.map(d => d.label))             // Values that can be found in data
+    .range([marginLeft, width - marginRight])  // Position (in pixel) to map data on screen
+    .padding(0.2);
+
 const y = d3.scaleLinear()
-    .domain([0, 100])
-    .range([height - marginBottom, marginTop]);
+    .domain([0, d3.max(data, d => d.value)])        // Not clear how d3.max() works
+    .range([height - marginBottom, marginTop]);     // SGV maps 0,0 in top left. Bottom left corner has
+                                                    // a "high" pixel position
 
-// Create the SVG container.
-const svg = d3.create("svg")
-    .attr("width", width)
-    .attr("height", height);
+// Add X axis
+svg.append('g')
+    .attr('transform', `translate(0,${height - marginBottom})`)     // Not clear what this is doing
+    .call(d3.axisBottom(x));                                        // Draw axis with given scale on bottom
 
-// Add the x-axis.
-svg.append("g")
-    .attr("transform", `translate(0,${height - marginBottom})`)
-    .call(d3.axisBottom(x));
-
-// Add the y-axis.
-svg.append("g")
-    .attr("transform", `translate(${marginLeft},0)`)
+// Add Y axis
+svg.append('g')
+    .attr('transform', `translate(${marginLeft},0)`)    // Not clear what this is doing
     .call(d3.axisLeft(y));
 
-// Append the SVG element.
-container.append(svg.node());
+// Create bars using ONLY enter()
+svg.selectAll('.bar')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('class', 'bar')
+    .attr('x', d => x(d.label))
+    .attr('y', d => y(d.value))
+    .attr('width', x.bandwidth())
+    .attr('height', d => y(0) - y(d.value))
+    .attr('fill', 'steelblue');
+
+container.append(svg.node());   // This is magic. What is container?
+
+const text_graph = d3.create('svg')
+    .attr('width', width)
+    .attr('height', height)
+
+const letters = ['a', 'b', 'c', 'd']
+
+text_graph.selectAll('.myChars')    // Create a new selection (now it's empty)
+    .data(letters)                  // Bind data to the selection
+    .enter()                        // Magic method that creates new elemens to selection for every new data element
+    .append('text')                 // Add a SVG text element for each node
+    .attr('x', (_d, i) => i * 30)   // Set the X position for each element
+    .attr('y', _d => 30)            // Set the Y position for each element
+    .text(d => d)
+
+container.append(text_graph.node())
