6e1553f75b96c216543f7d9ce478d43453c4efb5
riccardomearelli <129194485+riccardomearelli@users.noreply.github.com>
2025-12-19 12:00:16 +0100

    Alt version of map
    Continuous color scale version of the map added.

--- app/webpack/src/script/main.js
+++ app/webpack/src/script/main.js
@@ -2,5 +2,7 @@ import '../style/main.css';
 
 // import mainSilvioExample from './silvioExample.js'
 // mainSilvioExample()
-import mainMap from './map.js';
-mainMap();
+// import mainMap from './map.js';
+// mainMap();
+import mainMapAlt from './mapAlt.js';
+mainMapAlt();
